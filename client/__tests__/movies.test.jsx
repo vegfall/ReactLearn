845a75bc33807635af11c7b96971ac6ec2f8b0f9
Vegard Falldalen <vegfall@gmail.com>
2023-11-07 17:56:41 +0100

    Added prettier

--- client/__tests__/movies.test.jsx
+++ client/__tests__/movies.test.jsx
@@ -1,61 +1,65 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import renderer, { act } from "react-test-renderer";
 
-import {ListMovies, NewMovieForm} from "../application";
-import {Simulate} from "react-dom/test-utils";
+import { ListMovies, NewMovieForm } from "../application";
+import { Simulate } from "react-dom/test-utils";
 
 describe("movies application", () => {
-    it('should show movie list', () => {
-        expect(6 * 9).toEqual(54);
+  it("should show movie list", () => {
+    expect(6 * 9).toEqual(54);
+  });
+
+  it("renders correctly", () => {
+    const component = renderer.create(<h1>Hello world!</h1>);
+    expect(component).toMatchSnapshot();
+  });
+
+  it("matches snapshot", async () => {
+    let component;
+
+    await act(async () => {
+      component = renderer.create(<ListMovies />);
     });
 
-    it("renders correctly", () => {
-        const component = renderer.create(<h1>Hello world!</h1>);
-        expect(component).toMatchSnapshot();
+    expect(component).toMatchSnapshot();
+  });
+
+  it("show new movie form", async () => {
+    let component;
+
+    await act(async () => {
+      component = renderer.create(<NewMovieForm />);
     });
 
-    it("matches snapshot", async () => {
-        let component;
+    expect(component).toMatchSnapshot();
+  });
 
-        await act(async () => {
-            component = renderer.create(<ListMovies/>);
-        });
+  it("submit new movie", async () => {
+    const onAddMovieMock = jest.fn(); //fn is a mock function
+    let component;
 
-        expect(component).toMatchSnapshot();
+    await act(async () => {
+      component = renderer.create(<NewMovieForm onAddMovie={onAddMovieMock} />);
     });
 
-    it("show new movie form", async () => {
-        let component;
-
-        await act(async () => {
-            component = renderer.create(<NewMovieForm/>);
-        });
-
-        expect(component).toMatchSnapshot();
+    //console.log(component.root.children[0]);
+    Simulate.change(component.root.children[0].children[1].children[1], {
+      target: { value: "Movie 1" },
     });
+    Simulate.change(component.root.children[0].children[2].children[1], {
+      target: { value: "2022" },
+    });
+    Simulate.submit(component.root.children[0]);
 
-    it("submit new movie", async () => {
-        const onAddMovieMock = jest.fn(); //fn is a mock function
-        let component;
+    //Simulate.input(component.querySelector("[data-testid=title]"), {target: {value: "Movie 1"}});
+    //Simulate.input(component.querySelector("[data-testid=year]"), {target: {value: "2022"}});
+    //Simulate.submit(component.querySelector("form"));
 
-        await act(async () => {
-            component = renderer.create(<NewMovieForm onAddMovie={onAddMovieMock}/>);
-        });
-
-        //console.log(component.root.children[0]);
-        Simulate.change(component.root.children[0].children[1].children[1], {target: {value: "Movie 1"}});
-        Simulate.change(component.root.children[0].children[2].children[1], {target: {value: "2022"}});
-        Simulate.submit(component.root.children[0])
-
-        //Simulate.input(component.querySelector("[data-testid=title]"), {target: {value: "Movie 1"}});
-        //Simulate.input(component.querySelector("[data-testid=year]"), {target: {value: "2022"}});
-        //Simulate.submit(component.querySelector("form"));
-
-        /*
+    /*
         expect(onAddMovieMock).toHaveBeenCalledWith({
             title: "Movie 1", year: "2022", plot: ""
         });
 
          */
-    });
+  });
 });
--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {ListMovies} from "./application";
+import { ListMovies } from "./application";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<ListMovies/>);
+root.render(<ListMovies />);
