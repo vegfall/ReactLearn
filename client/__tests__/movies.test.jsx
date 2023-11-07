f2172eff48dca64876eb052f0d0ffa46a1679fc3
Vegard Falldalen <vegfall@gmail.com>
2023-11-07 16:05:46 +0100

    Update snapshot & refactoring code

--- client/__tests__/movies.test.jsx
+++ client/__tests__/movies.test.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import renderer, { act } from "react-test-renderer";
 
 import {ListMovies} from "../application";
 
 describe("movies application", () => {
     it('should show movie list', () => {
-        //const element = document.createElement("div");
-        //const root = ReactDOM.createRoot(element);
-        //root.render(<ListMovies/>);
-        //expect(element.querySelector("h1").innerHTML).toEqual("List movies!");
-
         expect(6 * 9).toEqual(54);
     });
 
