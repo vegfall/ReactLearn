75741af968e70b1b152a5c1344e85fe60e407f9a
Vegard Falldalen <vegfall@gmail.com>
2023-11-07 17:15:23 +0100

    Added simulated test... not working

--- client/__tests__/movies.test.jsx
+++ client/__tests__/movies.test.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {Component} from "react";
 import renderer, { act } from "react-test-renderer";
 
-import {ListMovies} from "../application";
+import {ListMovies, NewMovieForm} from "../application";
+import {Simulate} from "react-dom/test-utils";
 
 describe("movies application", () => {
     it('should show movie list', () => {
@@ -21,5 +22,37 @@ describe("movies application", () => {
         });
 
         expect(component).toMatchSnapshot();
+    });
+
+    it("show new movie form", async () => {
+        let component;
+
+        await act(async () => {
+            component = renderer.create(<NewMovieForm/>);
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it("submit new movie", async () => {
+        const onAddMovieMock = jest.fn(); //fn is a mock function
+        let component;
+
+        await act(async () => {
+            component = renderer.create(<NewMovieForm onAddMovie={onAddMovieMock}/>);
+        });
+
+        //console.log(component.root.children[0]);
+        Simulate.change(component.root.children[0].children[1].children[1], {target: {value: "Movie 1"}});
+        Simulate.change(component.root.children[0].children[2].children[1], {target: {value: "2022"}});
+        Simulate.submit(component.root.children[0])
+
+        //Simulate.input(component.querySelector("[data-testid=title]"), {target: {value: "Movie 1"}});
+        //Simulate.input(component.querySelector("[data-testid=year]"), {target: {value: "2022"}});
+        //Simulate.submit(component.querySelector("form"));
+
+        expect(onAddMovieMock).toHaveBeenCalledWith({
+            title: "Movie 1", year: "2022", plot: ""
+        });
     });
 });
--- client/application.jsx
+++ client/application.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 export function ListMovies() {
     return <div>
-        <h1>List movies!</h1>;
+        <h1>List movies!</h1>
         <ul>
             <li>Movie 1</li>
             <li>Movie 2</li>
         </ul>
     </div>
+}
+
+export function NewMovieForm({onAddMovie}) {
+    const[title, setTitle] = useState("");
+    const[year, setYear] = useState("");
+    const[plot, setPlot] = useState("");
+
+    function handleSubmit() {
+        onAddMovie({title, year, plot});
+    }
+
+    return <form onSubmit={handleSubmit}>
+        <h1>Add new movie</h1>
+        <div>
+            Title: <input type="text" data-testid="title" value={title} onChange={e => setTitle(e.target.value)}/>
+        </div>
+        <div>
+            Year: <input type="text" data-testid="year" value={year} onChange={e => setTitle(e.target.value)}/>
+        </div>
+        <div>
+            Plot:
+            <div><textarea data-testid="plot" value={plot} onChange={e => setPlot(e.target.value)}/></div>
+        </div>
+    </form>
 }
